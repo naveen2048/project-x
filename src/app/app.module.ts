3ba7155738f537902f4f481c5a96598162eb6054
naveen.2048@gmail.com <naveen.2048@gmail.com>
2018-09-02 14:01:45 -0500

    added  header, content and footer components and included bootstrap 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { rootRoutes } from './app.routing';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(rootRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
