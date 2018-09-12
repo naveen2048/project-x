4b54c2f5e23958908feec822b924f5441d0d7542
naveen.2048@gmail.com <naveen.2048@gmail.com>
2018-09-12 09:55:25 -0500

    changed toolbar color and removed items. Enabled navigation for 2 modules

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalVariable } from '../../global.variables';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  imgLoading:string = GlobalVariable.LOAD_IMAGE;
   constructor() { }
 
   ngOnInit() {
   }
-
 }
