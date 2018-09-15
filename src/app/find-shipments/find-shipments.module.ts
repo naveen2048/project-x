141f090836069da049b14e338ee43b6b3af3b305
unknown <sreekanthjo@gmail.com>
2018-09-16 03:21:14 +0530

    auto complete and accordion changes.

--- src/app/find-shipments/find-shipments.module.ts
+++ src/app/find-shipments/find-shipments.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComponentsFindShipments } from './index';
-
+import {AgmCoreModule} from '@agm/core'
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    ComponentsFindShipments
+    ComponentsFindShipments,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey:"AIzaSyDyjSfwQglI1lO4t8Mg1H0D-bZXtjUMsIs",
+      libraries:["places"]
+    })
   ],
   declarations: []
 })
