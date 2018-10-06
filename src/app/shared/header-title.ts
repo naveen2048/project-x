060f1178bb078b3e1e91dd85c93eb061312f8fa0
naveen.2048@gmail.com <naveen.2048@gmail.com>
2018-10-06 13:19:46 -0500

    changed background color to gray instead of yellow. Need to fix color issue around breadcrum

--- src/app/shared/header-title.ts
+++ src/app/shared/header-title.ts
@@ -6,13 +6,24 @@ import { Component, Input } from "@angular/core";
   <div class="row">
   <div class="col-sm-12">
   <h2>{{title | uppercase}}</h2>
+  <p class="helperText">{{caption}}</p>
   <hr/>
   </div>
   </div>
   `,
-  styleUrls: []
+  styles: [
+    ` .helperText {
+        color: #999;
+        font-size: 1.875rem;
+        letter-spacing: 0.025rem;
+        line-height: 1.125rem;
+      }
+    `
+  ]
 })
 export class HeaderTitleComponent {
   @Input()
   title: string;
+  @Input()
+  caption: string;
 }
